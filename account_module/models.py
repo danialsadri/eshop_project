f88257ea27e2b789d010d8e57a501208ed108cdb
danial sadri <danielsadri01@gmail.com>
2023-12-17 18:23:34 +0330

    update

---
+++ account_module/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='images/profile', verbose_name='تصویر آواتار', null=True, blank=True)
+    email_active_code = models.CharField(max_length=100, verbose_name='کد فعالسازی ایمیل')
+    about_user = models.TextField(null=True, blank=True, verbose_name='درباره شخص')
+    address = models.TextField(null=True, blank=True, verbose_name='آدرس')
+
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
+
+    def __str__(self):
+        if self.first_name is not None and self.last_name is not None:
+            return self.get_full_name()
+        return self.email
---
+++ eshop_project/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', include('home_module.urls')),
+    path('', include('account_module.urls')),
+    path('articles/', include('article_module.urls')),
+    path('contact_us/', include('contact_module.urls')),
+    path('products/', include('product_module.urls')),
+    path('user/', include('user_panel_module.urls')),
+    path('order/', include('order_module.urls')),
+    path('admin/', admin.site.urls),
+    path('admin_panel/', include('admin_panel.urls'))
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
