789fead63fd2908bbc058ab02946bcd7bc57dcf6
danial sadri <danielsadri01@gmail.com>
2024-01-01 10:17:34 +0330

    update

--- article_module/urls.py
+++ article_module/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'articles'
 urlpatterns = [
-    path('', views.ArticlesListView.as_view(), name='articles_list'),
-    path('cat/<str:category>', views.ArticlesListView.as_view(), name='articles_by_category_list'),
-    path('<pk>/', views.ArticleDetailView.as_view(), name='articles_detail'),
-    path('add-article-comment', views.add_article_comment, name='add_article_comment')
+    path('list/', views.ArticlesListView.as_view(), name='articles_list'),
+    path('list/cat/<str:category>/', views.ArticlesListView.as_view(), name='articles_by_category_list'),
+    path('detail/<pk>/', views.ArticleDetailView.as_view(), name='articles_detail'),
+    path('add-article-comment/', views.add_article_comment, name='add_article_comment'),
 ]
--- article_module/views.py
+++ article_module/views.py
@@ -35,16 +35,9 @@ class ArticleDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(ArticleDetailView, self).get_context_data()
         article: Article = kwargs.get('object')
-        context['comments'] = ArticleComment.objects.filter(article_id=article.id, parent=None).order_by(
-            '-create_date').prefetch_related('articlecomment_set')
-        context['comments_count'] = ArticleComment.objects.filter(article_id=article.id).count()
+        context['comments'] = ArticleComment.objects.filter(article_id=article.id, parent=None).order_by('-create_date').prefetch_related('articlecomment_set')
+        context['comments_count'] = ArticleComment.objects.filter(article_id=article.id).count(),
         return context
-
-
-def article_categories_component(request: HttpRequest):
-    article_main_categories = ArticleCategory.objects.prefetch_related('articlecategory_set').filter(is_active=True, arent_id=None)
-    context = {'main_categories': article_main_categories}
-    return render(request, 'article_module/components/article_categories_component.html', context)
 
 
 def add_article_comment(request: HttpRequest):
@@ -53,12 +46,16 @@ def add_article_comment(request: HttpRequest):
         article_comment = request.GET.get('article_comment')
         parent_id = request.GET.get('parent_id')
         print(article_id, article_comment, parent_id)
-        new_comment = ArticleComment(article_id=article_id, text=article_comment, user_id=request.user.id, parent_id=parent_id)
-        new_comment.save()
+        ArticleComment.objects.create(article_id=article_id, text=article_comment, user_id=request.user.id, parent_id=parent_id)
         context = {
-            'comments': ArticleComment.objects.filter(article_id=article_id, parent=None).order_by(
-                '-create_date').prefetch_related('articlecomment_set'),
-            'comments_count': ArticleComment.objects.filter(article_id=article_id).count()
+            'comments': ArticleComment.objects.filter(article_id=article_id, parent=None).order_by('-create_date').prefetch_related('articlecomment_set'),
+            'comments_count': ArticleComment.objects.filter(article_id=article_id).count(),
         }
         return render(request, 'article_module/includes/article_comments_partial.html', context)
     return HttpResponse('response')
+
+
+def article_categories_component(request: HttpRequest):
+    article_main_categories = ArticleCategory.objects.prefetch_related('articlecategory_set').filter(is_active=True, parent_id=None)
+    context = {'main_categories': article_main_categories}
+    return render(request, 'article_module/components/article_categories_component.html', context)
